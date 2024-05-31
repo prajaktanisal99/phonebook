37d6140bedae5dd2407a57912f5abf118e4cacff
Prajakta Nisal <prajaktanisal99@gmail.com>
2024-05-31 01:48:57 -0500

    Minor change

--- app/page.tsx
+++ app/page.tsx
@@ -2,25 +2,17 @@
 import React from "react";
 import { signIn, useSession } from "next-auth/react";
 import Dashboard from "./components/Dashboard";
+import { UserSchema } from "@/types/types";
 
 const Home = () => {
-  const { data: session } = useSession();
 
-  console.log("data", session);
+  const user: UserSchema = { name: "John Doe", email: "johndoe@gmail.com"}
   return (
     <div>
-      {session?.user ? (
+
         <div className="p-4 md:p-10 flex flex-col md:flex-row justify-center gap-3 w-screen h-screen text-black overflow-hidden">
-          <Dashboard user={session.user} />
+          <Dashboard user={user} />
         </div>
-      ) : (
-        <div className="flex flex-col p-10 h-screen">
-          <h1 className="text-3xl ">Not Signed In</h1>
-          <button className="border-2 w-fit p-4 rounded-sm " onClick={() => signIn()}>
-            Sign In
-          </button>
-        </div>
-      )}
     </div>
   );
 };
